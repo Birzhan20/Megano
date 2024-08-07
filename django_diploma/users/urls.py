654f1318383f85d37cb247adc202f96a16637ebe
Alexey <alexei@bk.ru>
2024-08-07 15:24:46 +0300

    example

--- django_diploma/users/urls.py
+++ django_diploma/users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SignInApiView
 
 urlpatterns = [
-    path('sign-in/', SignInApiView.as_view(), name='sign-in'),
+    path('sign-in', SignInApiView.as_view(), name='sign-in'),
 ]
--- django_diploma/users/views.py
+++ django_diploma/users/views.py
@@ -1,18 +1,29 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+import json
+
+from django.contrib.auth import authenticate, login
 from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import SignInSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 class SignInApiView(APIView):
     def post(self, request):
-        serializer = SignInSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.validated_data['user']
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            }, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        user_data = json.loads(request.body)
+        username = user_data.get("username")
+        password = user_data.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        # serializer = SignInSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     user = serializer.validated_data['user']
+        #     refresh = RefreshToken.for_user(user)
+        #     return Response({
+        #         'refresh': str(refresh),
+        #         'access': str(refresh.access_token),
+        #     }, status=status.HTTP_200_OK)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
